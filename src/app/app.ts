e1a42a0304f3e83aff95b65266c12425c111ec98
Madina Askar Zada <madin.aska@gmail.com>
2025-08-22 13:47:40 +0200

    Build the Project Structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Home } from './main-content/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Header,
+    Home
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/main-content/my-work/my-work.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-work',
+  imports: [],
+  templateUrl: './my-work.html',
+  styleUrl: './my-work.scss'
+})
+export class MyWork {
+
+}
