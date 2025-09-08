2cf7120b364b8f6066b9bd4bc4f12366103d6f8f
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:41:12 +0200

    hero section added

--- src/app/main-content/home/home.ts
+++ src/app/main-content/home/home.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { socialLinks } from '../../social-links';
+import { TranslateModule } from '@ngx-translate/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
+  links = socialLinks;
 
+  private viewportScroller = inject(ViewportScroller);
+
+  scrollTo(sectionId: string) {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
