b9d91efdd4bd0da704391eeb5196d761f6aff7d0
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:44:07 +0200

    footer section added, shared folder added for the footer and header section

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { socialLinks } from '../../social-links';
+import { RouterModule } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
+  links = socialLinks;
 
+  private viewportScroller = inject(ViewportScroller);
+
+  scrollTo(sectionId: string) {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
