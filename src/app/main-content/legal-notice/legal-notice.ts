fafe6a51f947646514a55f4b88232f5eeae74086
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:45:07 +0200

    legal notice page added

---
+++ src/app/main-content/legal-notice/legal-notice.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from "../../shared/header/header";
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [Header, TranslateModule],
+  templateUrl: './legal-notice.html',
+  styleUrl: './legal-notice.scss'
+})
+export class LegalNotice {
+
+}
