ff5895da2c6541a4812255ad5db7e6270a274706
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:38:17 +0200

    social links added

---
+++ src/app/social-links.ts
@@ -0,0 +1,5 @@
+export const socialLinks = {
+  github: 'https://github.com/madina-aska',
+  linkedin: 'https://www.linkedin.com/in/madina-askar-zada-396249156/',
+  email: 'madin.aska@gmail.com',
+};
