301684572b070106bd9610cf78785cd6b9336d90
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:40:44 +0200

    about me section added

--- src/app/main-content/about-me/about-me.ts
+++ src/app/main-content/about-me/about-me.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss'
 })
 export class AboutMe {
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  scrollToContact() {
+    this.viewportScroller.scrollToAnchor('contact');
+  }
 
 }
