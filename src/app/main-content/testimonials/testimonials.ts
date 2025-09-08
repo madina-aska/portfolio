4cb3638c68cd5d98b175e0f14b8a4e9dbb32904e
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:42:43 +0200

    testimonial section are added

--- src/app/main-content/testimonials/testimonials.ts
+++ src/app/main-content/testimonials/testimonials.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './testimonials.html',
-  styleUrl: './testimonials.scss'
+  styleUrl: './testimonials.scss',
 })
 export class Testimonials {
+  @ViewChild('slider') slider!: ElementRef<HTMLDivElement>;
 
+  private startX = 0;
+  private scrollLeft = 0;
+
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].pageX;
+    this.scrollLeft = this.slider.nativeElement.scrollLeft;
+  }
+
+  onTouchMove(event: TouchEvent) {
+    const x = event.touches[0].pageX;
+    const walk = this.startX - x;
+    this.slider.nativeElement.scrollLeft = this.scrollLeft + walk;
+  }
+
+  onTouchEnd() {}
 }
