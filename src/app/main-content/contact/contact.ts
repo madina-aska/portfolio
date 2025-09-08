6ce461306ec3c3dbd81a771776cd9b11d00dd03a
Madina Askar Zada <madin.aska@gmail.com>
2025-09-08 14:44:33 +0200

    contact form added

--- src/app/main-content/contact/contact.ts
+++ src/app/main-content/contact/contact.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { ViewportScroller } from '@angular/common';
+import { socialLinks } from '../../social-links';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, TranslateModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  styleUrl: './contact.scss',
 })
 export class Contact {
+  links = socialLinks;
 
+  private viewportScroller = inject(ViewportScroller);
+
+  scrollTo(sectionId: string) {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
+
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacy: false,
+  };
+
+  // mailTest = true;
+
+  post = {
+    endPoint: 'http://madina-askarzada.com/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    // if (ngForm.valid && ngForm.submitted) {
+    // console.log(this.contactData);
+    // }
+    if (ngForm.submitted && ngForm.form.valid) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid) {
+      ngForm.resetForm();
+    }
+  }
 }
