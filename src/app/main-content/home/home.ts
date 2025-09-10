9f52a400e19e86fc24e2988a49c4a9fa8a83f066
Madina Askar Zada <madin.aska@gmail.com>
2025-09-10 23:08:37 +0200

    responsive feature added

--- src/app/main-content/home/home.ts
+++ src/app/main-content/home/home.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { socialLinks } from '../../social-links';
-import { TranslateModule } from '@ngx-translate/core';
-import { ViewportScroller } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule, ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -16,5 +16,17 @@ export class Home {
 
   scrollTo(sectionId: string) {
     this.viewportScroller.scrollToAnchor(sectionId);
+  }
+
+
+   currentLang: string;
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || 'en';
+    
+    // Optional: Listen for language changes
+    this.translate.onLangChange.subscribe(event => {
+      this.currentLang = event.lang;
+    });
   }
 }
