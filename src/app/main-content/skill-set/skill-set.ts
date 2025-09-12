79edcad70fa26a420467fff38102a875e49c4022
Madina Askar Zada <madin.aska@gmail.com>
2025-09-12 13:49:24 +0200

    translation feature added to the skill set section

--- src/app/main-content/skill-set/skill-set.ts
+++ src/app/main-content/skill-set/skill-set.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skill-set',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './skill-set.html',
   styleUrl: './skill-set.scss'
 })
